b86defe1c407b1a0e9fb217a43f2f9a9d00d7fdf
jwei <wade30191@gmail.com>
2016-07-22 19:30:20 +0800

    Some revisions of showorder.js

--- Phil-POS/WebContent/js/showorder.js
+++ Phil-POS/WebContent/js/showorder.js
@@ -1,28 +1,24 @@
 
-var orderLists;
-
-function getOrderLists(oid) {
-	var result;
-		    $.ajax({
-		        url: '192.168.1.158:8080/Phil-POS/orderLists',
-		        type: 'get',
-		        data: {
-		        	oid : oid
-		        },
-		        dataType: 'json',
-		         async: false,
-		        success: function (data) {
-		            result = data;
-		        }
-		    });
-		return result;
-	}
-
 $(document).ready(function(){
 	$(".test2").on('click',function(){
 		var oid = this.value;
-		orderLists = getOrderLists(oid);
-		console.log(orderLists);
+		
+		$.ajax({
+	        url: 'orderLists',
+	        type: 'get',
+	        data: {
+	        	oid : oid
+	        },
+	        dataType: 'json',
+	        success: function (data) {
+	            var orderLists = data;
+	            console.log(orderLists);
+	            
+	            // 將結果顯示於畫面
+	        }
+	    });
+		
+		// 其他工作
 
 	});
 });
