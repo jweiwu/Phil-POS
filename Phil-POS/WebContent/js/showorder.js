0d7fb18d5ca1b5f75ee54863578110c8b0651d8e
洪士凱 <prodigygod0209@gmail.com>
2016-08-04 14:47:05 +0800

    showorder

--- Phil-POS/WebContent/js/showorder.js
+++ Phil-POS/WebContent/js/showorder.js
@@ -1,24 +1,47 @@
+var test = {"orderLists":[{"meal":"牛肉漢堡","mid":6,"oid":30,"price":20,"quantity":1},{"meal":"豬肉漢堡","mid":7,"oid":30,"price":100,"quantity":1},{"meal":"雞排漢堡","mid":8,"oid":30,"price":110,"quantity":1}]};
 
 $(document).ready(function(){
-	$(".test2").on('click',function(){
-		var oid = this.value;
+	 // $("button").click(function () {
+        
+  //    });
+	$(".order-detail").on('click',function(){
+		 $(".pop").fadeIn(300);
+         // positionPopup();
+		for(i=0 ;i<test.orderLists.length;i++){
+			let meal = test.orderLists[i].meal;
+			let price = test.orderLists[i].price;
+			let quantity = test.orderLists[i].quantity
+
+			$(".pop table").append("<tr class='delete'><td>"+meal+"</td><td>"+quantity+"</td><td>"+price+"</td></tr>");
+		}
+
 		
-		$.ajax({
-	        url: 'orderLists',
-	        type: 'get',
-	        data: {
-	        	oid : oid
-	        },
-	        dataType: 'json',
-	        success: function (data) {
-	            var orderLists = data;
-	            console.log(orderLists);
+		
+		// $.ajax({
+	 //        url: 'http://192.168.1.158:8080/Phil-POS/orderLists',
+	 //        type: 'get',
+	 //        data: {
+	 //        	oid : oid
+	 //        },
+	 //        dataType: 'json',
+	 //        success: function (data) {
+	 //            var orderLists = data;
+	 //            console.log(orderLists);
 	            
-	            // 將結果顯示於畫面
-	        }
-	    });
+	 //            // 將結果顯示於畫面
+	 //        }
+	 //    });
 		
 		// 其他工作
 
 	});
 });
+
+
+	
+
+     $(".pop > span, .pop").click(function () {
+         $(".pop").fadeOut(300);
+         $('.delete').remove();
+     });
+
