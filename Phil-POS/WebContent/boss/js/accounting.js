749694c677c488ebd3df7adaa507a04f93df5d37
洪士凱 <prodigygod0209@gmail.com>
2016-08-06 18:03:56 +0800

    day pick

--- Phil-POS/WebContent/boss/js/accounting.js
+++ Phil-POS/WebContent/boss/js/accounting.js
@@ -1,5 +1,10 @@
 
+
+var month = []
+
 $(document).ready( function () {
+
+
 
     var template = Handlebars.templates.nav;
     $('#nav-template').html(template);
@@ -7,8 +12,24 @@ $(document).ready( function () {
     var year = 2016;
     var month = 8;
     var day = 1;
+
+    $('.daypick').on('click',function(){
+		var m = $("#date").val().split("-",3);;
+		
+	 
+		for(let i = 0; i<m.length;i++){
+			m[i] = parseInt(m[i]);
+		}
+
+		year = m[0];
+		month =m[1];
+		day = m[2];
+
+		 getAccData(year, month, day);
+	
+	});
     
-    getAccData(year, month);
+
     
 });
 
