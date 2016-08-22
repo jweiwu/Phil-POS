89a5fb58ec1ccd49b2a16819af77ea202dac21c3
洪士凱 <prodigygod0209@gmail.com>
2016-08-22 17:02:24 +0800

    2

--- Phil-POS/WebContent/js/order.js
+++ Phil-POS/WebContent/js/order.js
@@ -104,29 +104,29 @@ function RemoverAllOrder(){
 	 * Import order list.
 	 * 
 	 */
-	// function getMenu() {
-	// 	var result;
+	function getMenu() {
+		var result;
 		
-	// 	    $.ajax({
-	// 	        url: 'showMenu',
-	// 	        type: 'get',
-	// 	        dataType: 'json',
-	// 	        async: false,
-	// 	        success: function (data) {
-	// 	            result = data;
-	// 	        }
-	// 	    });
+		    $.ajax({
+		        url: 'showMenu',
+		        type: 'get',
+		        dataType: 'json',
+		        async: false,
+		        success: function (data) {
+		            result = data;
+		        }
+		    });
 		    
-	// 	return result;
-	// }
+		return result;
+	}
 	
 	
 //get ID 
 var jsTest = document.getElementById('js-test');
 var allpriceNum = document.getElementById('allpriceNum');
 $(document).ready(function(){
-	 result = {"heads":[{"createtime":"2016-04-30 21:02:12.0","hid":1,"item":"漢堡"},{"createtime":"2016-04-30 21:05:35.0","hid":2,"item":"飲料"},{"createtime":"2016-05-11 14:19:23.0","hid":9,"item":"點心"}],"meals":[{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"雞排漢堡","mid":8,"price":110},{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"牛肉漢堡","mid":6,"price":20},{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"豬肉漢堡","mid":7,"price":100},{"active":1,"discount":1.0,"head":"飲料","hid":2,"meal":"可口可樂","mid":9,"price":30},{"active":1,"discount":1.0,"head":"飲料","hid":2,"meal":"雪碧","mid":10,"price":30},{"active":1,"discount":1.0,"head":"點心","hid":9,"meal":"薯條","mid":11,"price":20},{"active":1,"discount":1.0,"head":"點心","hid":9,"meal":"雞塊","mid":12,"price":25}]};
-	// result = getMenu();
+	 // result = {"heads":[{"createtime":"2016-04-30 21:02:12.0","hid":1,"item":"漢堡"},{"createtime":"2016-04-30 21:05:35.0","hid":2,"item":"飲料"},{"createtime":"2016-05-11 14:19:23.0","hid":9,"item":"點心"}],"meals":[{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"雞排漢堡","mid":8,"price":110},{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"牛肉漢堡","mid":6,"price":20},{"active":1,"discount":1.0,"head":"漢堡","hid":1,"meal":"豬肉漢堡","mid":7,"price":100},{"active":1,"discount":1.0,"head":"飲料","hid":2,"meal":"可口可樂","mid":9,"price":30},{"active":1,"discount":1.0,"head":"飲料","hid":2,"meal":"雪碧","mid":10,"price":30},{"active":1,"discount":1.0,"head":"點心","hid":9,"meal":"薯條","mid":11,"price":20},{"active":1,"discount":1.0,"head":"點心","hid":9,"meal":"雞塊","mid":12,"price":25}]};
+	result = getMenu();
 	showHead();
 
 	 // 操作DOM
